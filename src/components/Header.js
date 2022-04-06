87e1e6789ea8c1a4d001dfbbd8f880c5e3c7ba38
Jeff Lyons <jeffreylyons@outlook.com>
2022-04-06 17:01:20 -0400

    commit 60

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,24 +5,32 @@ import "../styles/header.css";
 import LogoutButton from "./LogoutButton";
 import OrderButton from "./OrderButton";
 import LoginBanner from "./LoginBanner";
+import { Navigate, useNavigate } from "react-router-dom";
 
 function Header() {
+  const navigate = useNavigate();
+
   return (
     <header>
       <nav className="header-nav">
         <div className="header-links">
           <div className="header-left">
-            <Link to="/donuts" className="link-header">
+            {/* <Link to="/donuts" className="link-header">
               <h1 className="donut-menu">Donuts</h1>
             </Link>
             <Link to="/drinks" className="link-header">
               <h1>Drinks</h1>
-            </Link>
+            </Link> */}
+            <button className="new-drink-button" onClick={() => navigate("/donuts")}>Donuts</button>
+            <button className="new-drink-button" onClick={() => navigate("/drinks")}>Drinks</button>
           </div>
 
           <div className="header-mid">
             <Link to="/">
-              <img src='https://res.cloudinary.com/redhero/image/upload/v1648940799/Donuts/donut-logo_miqpyr.png' className="logo" />
+              <img
+                src="https://res.cloudinary.com/redhero/image/upload/v1648940799/Donuts/donut-logo_miqpyr.png"
+                className="logo"
+              />
             </Link>
           </div>
 
