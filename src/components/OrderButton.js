4527ff7d1d83a2e4bc5132b8f85c85def98f663e
Jeff Lyons <jeffreylyons@outlook.com>
2022-04-07 01:31:35 -0400

    v3.3

--- src/components/OrderButton.js
+++ src/components/OrderButton.js
@@ -1,16 +1,16 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+import { Navigate, useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 import "../styles/header.css";
 
 function OrderButton() {
   const { user, isAuthenticated } = useAuth0();
+  const navigate = useNavigate();
 
   return (
     isAuthenticated && (
-      <Link to="/order" className="placeorder-btn">
-        <h1>Place Order</h1>
-      </Link>
+      <button className="placeorder-btn" onClick={() => navigate('/order')}>Place Order</button>
     )
   );
 }
